48667c6f97d838f067c1c4a92d45b9c9c221a78b
NEGIRSN <negirajendras@gmail.com>
2018-10-13 14:53:58 +0800

    Update 01 Todolist

---
+++ RS02/ToDoList/RSTasks.py
@@ -0,0 +1,19 @@
+class Task:
+    # This is the class constructor
+    def __init__(self):
+        self.taskname = ""
+        self.taskdesc = ""
+
+        # Class Method: set_taskname
+        # INPUT: inName
+        # EXPORT: None
+        # Purpose: Set the name of the task
+        def set_taskname(self, inName):
+            self.taskname = inName
+
+        # Class Method: set_taskdesc
+        # INPUT: inDesc
+        # EXPORT: None
+        # Purpose: Set the description of the task
+        def set_taskdesc(self, inDesc):
+            self.taskdesc = inDesc
