cb64027f6ac1b41606bf71dca76ab6d6b1ab3a3e
RSN <negirajendras@gmail.com>
2018-10-20 15:43:36 +0800

    Code improvement on TODO List 20Oct18

--- RS02/ToDoList/RSTasks.py
+++ RS02/ToDoList/RSTasks.py
@@ -8,12 +8,19 @@ class Task:
         # INPUT: inName
         # EXPORT: None
         # Purpose: Set the name of the task
-        def set_taskname(self, inName):
+    def set_taskname(self, inName):
             self.taskname = inName
 
         # Class Method: set_taskdesc
         # INPUT: inDesc
         # EXPORT: None
         # Purpose: Set the description of the task
-        def set_taskdesc(self, inDesc):
+    def set_taskdesc(self, inDesc):
             self.taskdesc = inDesc
+
+    def get_taskname(self):
+            return self.taskname
+
+    def get_taskdesc(self):
+            return self.taskdesc
+
